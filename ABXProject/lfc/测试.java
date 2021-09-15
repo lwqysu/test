f0a6c2a17854caad8deabc96b01f90638730465e
liuwenqiang <liuwenqiang@agree.com.cn>
2021-09-15 14:55:29 +0800

    提交java

---
+++ ABXProject/lfc/测试.java
@@ -0,0 +1,29 @@
+package ABXProject.lfc;
+
+
+import java.io.IOException;
+import cn.com.agree.ab.a5.runtime.sdk.annotation.LFC;
+import cn.com.agree.ab.a5.runtime.sdk.AbstractLFC;
+import cn.com.agree.ab.a5.runtime.sdk.Future;
+import cn.com.agree.ab.a5.runtime.sdk.annotation.EndNext;
+import cn.com.agree.ab.a5.runtime.sdk.annotation.Start;
+import cn.com.agree.ab.a5.runtime.sdk.annotation.Step;
+import cn.com.agree.ab.a5.runtime.sdk.enums.Out;
+
+/**
+ * @author
+ * @description 
+ */
+@LFC
+public class 测试 extends AbstractLFC
+{
+
+
+	@Start
+	@Step(name = "start")
+	public Future start() throws IOException
+	{
+		return Future.SUCCESS;
+	}
+
+}
