df490594e9941d961391221b686d52d25466e1bd
liuwenqiang <liuwenqiang@agree.com.cn>
2021-09-15 14:52:01 +0800

    提交ABX工程

---
+++ ABXProject/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  //eslint默认推荐规则
+  extends: ['eslint:recommended'],
+  env: {},
+  parser: 'babel-eslint', // 使用babel进行编译
+  plugins: [], //插件
+  parserOptions: {},
+  rules: {
+    //这里自定义规则，规则地址: http://eslint.cn/docs/rules/
+  }
+};
